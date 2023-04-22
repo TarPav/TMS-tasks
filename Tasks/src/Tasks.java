b9919f8d0eb9ee4d448411f11f0ef073995ad6eb
TarPav <pawataranda@gmail.com>
2023-04-22 13:59:26 +0200

    Second task complete

--- Tasks/src/Tasks.java
+++ Tasks/src/Tasks.java
@@ -1,20 +1,22 @@
 import java.util.Scanner;
 public class Tasks {
     public static void main(String args[]) {
-        int Grad;
-        int Min;
-        int Sek;
-        double Pi;
-        Pi = 3.14159;
+        int x1;
+        int y1;
+        int x2;
+        int y2;
         Scanner in = new Scanner(System.in);
-        System.out.println("Задайте угол в градусах");
-        Grad = in.nextInt();
-        System.out.println("Впишите минуты");
-        Min = in.nextInt();
-        System.out.println("Впишите секунды");
-        Sek = in.nextInt();
+        System.out.println("Введите значение x1");
+        x1 = in.nextInt();
+        System.out.println("Введите значение y1");
+        y1 = in.nextInt();
+        System.out.println("Введите значение x2");
+        x2 = in.nextInt();
+        System.out.println("Введите значение y2");
+        y2 = in.nextInt();
         double result;
-        result = (Grad * Pi / 180) + (Min * Pi / 10800) + (Sek * Pi / 648000);
-        System.out.println(result);
+        result = ((x2-x1) * (x2-x1)) + ((y2-y1) * (y2-y1));
+        double result1 = Math.sqrt(result);
+        System.out.println("Длина отрезка составляет " + result1);
     }
 }
