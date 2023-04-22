b23268bc7f6a11868ba6750e58ba33ddceb9526b
TarPav <pawataranda@gmail.com>
2023-04-22 15:19:53 +0200

    Third task comlete

--- Tasks/src/Tasks.java
+++ Tasks/src/Tasks.java
@@ -1,22 +1,28 @@
 import java.util.Scanner;
 public class Tasks {
     public static void main(String args[]) {
-        int x1;
-        int y1;
-        int x2;
-        int y2;
+        int h1;
+        int m1;
+        int h2;
+        int m2;
         Scanner in = new Scanner(System.in);
-        System.out.println("Введите значение x1");
-        x1 = in.nextInt();
-        System.out.println("Введите значение y1");
-        y1 = in.nextInt();
-        System.out.println("Введите значение x2");
-        x2 = in.nextInt();
-        System.out.println("Введите значение y2");
-        y2 = in.nextInt();
+        System.out.println("Введите час отправления");
+        h1 = in.nextInt();
+        System.out.println("Введите минуты отправления");
+        m1 = in.nextInt();
+        System.out.println("Введите час прибытия");
+        h2 = in.nextInt();
+        System.out.println("Введите минуты прибытия");
+        m2 = in.nextInt();
         double result;
-        result = ((x2-x1) * (x2-x1)) + ((y2-y1) * (y2-y1));
-        double result1 = Math.sqrt(result);
-        System.out.println("Длина отрезка составляет " + result1);
+        double result1;
+        result = ((h2 * 60 + m2) - (h1 * 60 + m1));
+        result1 = (((h2+24) * 60 + m2) - (h1 * 60 + m1));
+        if(h2>h1) {
+            System.out.println("Время в пути составит = " + result);
+        }
+        else{
+            System.out.println("Время в пути со сменой даты составит = " + result1);
+        }
     }
 }
